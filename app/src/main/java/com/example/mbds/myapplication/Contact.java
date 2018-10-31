79f02cee1302bb417ecf77d241a9d51dada73110
ThomasGauthierr <thomasg70360@gmail.com>
2018-10-31 15:25:05 +0100

    Contact buttons

--- app/src/main/java/com/example/mbds/myapplication/Contact.java
+++ app/src/main/java/com/example/mbds/myapplication/Contact.java
@@ -1,14 +1,28 @@
 package com.example.mbds.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Contact extends AppCompatActivity {
 
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+    }
+
+    public void addContact() {
+        System.out.println("Add contact");
+        // ToDo : Check if contact exists then add it to the local DB
+    }
+
+    public void back(View v) {
+        Intent intent = new Intent(this, FragmentExample.class);
+        startActivity(intent);
     }
 }
