1a3b7876f3a6ed14d42d3357dd06792a6767c29d
ThomasGauthierr <thomasg70360@gmail.com>
2019-01-06 20:37:53 +0100

    send message with api

--- app/src/main/java/com/example/mbds/myapplication/CreateMessage.java
+++ app/src/main/java/com/example/mbds/myapplication/CreateMessage.java
@@ -1,6 +1,8 @@
 package com.example.mbds.myapplication;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +11,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mbds.myapplication.services.DBHelper;
+import com.example.mbds.myapplication.services.PollService;
 import com.example.mbds.myapplication.services.entries.MessageEntry;
+
+import org.json.JSONObject;
 
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -21,6 +32,8 @@ import java.security.PublicKey;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -62,6 +75,55 @@ public class CreateMessage extends AppCompatActivity {
         String sender = "Me";
         String receiver = ((EditText)findViewById(R.id.receiver_edt)).getText().toString();
         String content = ((EditText)findViewById(R.id.content_edt)).getText().toString();
+
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("message", content);
+        params.put("receiver", receiver);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://baobab.tokidev.fr/api/sendMsg";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (url, new JSONObject(params),
+
+                        new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                SharedPreferences mPreferences = getSharedPreferences("session" ,MODE_PRIVATE);
+
+                                try {
+
+                                } catch (Exception e) {
+                                    Log.d("error send", e.getMessage());
+                                    return;
+                                }
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }) {
+            /** Passing some request headers* */
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+
+                SharedPreferences mPreferences = getSharedPreferences("session" ,MODE_PRIVATE);
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", "Bearer " + mPreferences.getString("token", ""));
+
+                return headers;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
+
 
         ContentValues vals = new ContentValues();
 
@@ -140,8 +202,6 @@ public class CreateMessage extends AppCompatActivity {
         byte[] decrypted = cipher.doFinal(encrypted);
         return decrypted;
     }
-
-
 
 
 }
